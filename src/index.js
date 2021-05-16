67b088daa66cebfb61bd5834404bca17c7584c5d
tiagosatur <tiagosatur@gmail.com>
2021-05-16 11:33:49 -0300

    Add post account route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const express = require("express");
-
+const { v4: uuid } = require("uuid");
 const app = express();
+app.use(express.json());
+
+let customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuid();
+
+  const newAccount = {
+    id,
+    cpf,
+    name,
+    statement: [],
+  };
+  customers.push(newAccount);
+  res.status(200).send(newAccount);
+});
 
 app.listen(3333, () => {
   console.log("🚀 Listening on port 3333");
